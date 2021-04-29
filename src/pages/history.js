a5679663cbbca50edf14c323683684869d81d136
Shawn Kohltfarber <shawn.kohltfarber@gmail.com>
2021-04-29 18:56:52 -0500

    Final push before demo.

--- src/pages/history.js
+++ src/pages/history.js
@@ -3,12 +3,14 @@ import Default from "../layout/default.js";
 import firebase from "../firebase/config";
 
 function useGuesses() {
-    const [guesees, setGuesses] = useState([]);
+    const [guesses, setGuesses] = useState([]);
 
     useEffect(() => {
         const unsubscribe = firebase
             .firestore()
             .collection('guess')
+            .orderBy("createdAt", "desc")
+            .limit(10)
             .onSnapshot((snapshot) => {
                 const newGuesses = snapshot.docs.map((doc) => ({
                     id: doc.id,
@@ -21,10 +23,12 @@ function useGuesses() {
         return () => unsubscribe();
     }, []);
 
-    return guesees;
+    return guesses;
 }
 
 export default function History() {
+
+
 
     const guesses = useGuesses();
 
@@ -32,7 +36,55 @@ export default function History() {
         <Default>
             <h1>History</h1>
 
-            <p>Here is the last 25 AI Spies for random people.</p>
+            <p>Here are AI Spies last 10 answers.</p>
+
+            <div className="container">
+                <div className="row">
+                    <div class="col-2 block-example border border-dark">
+                        Best Guess
+                    </div>
+                    <div class="col-2 block-example border border-dark">
+                        Confidence level
+                    </div>
+                    <div class="col-2 block-example border border-dark">
+                        Middle Guess
+                     </div>
+                    <div class="col-2 block-example border border-dark">
+                        Confidence level
+                    </div>
+                    <div class="col-2 block-example border border-dark">
+                        Worst Guess
+                    </div>
+                    <div class="col-2 block-example border border-dark">
+                        Confidence level
+                    </div>
+                </div>
+                {guesses.map((guess) => (
+                    <div key={guess.id} className="row">
+
+                        <div class="col-2 block-example border border-dark">
+                            {guess.guess_1}
+                        </div>
+                        <div class="col-2 block-example border border-dark">
+                            {(guess.probability_1 * 100).toFixed(2)}%
+                        </div>
+                        <div class="col-2 block-example border border-dark">
+                            {guess.guess_2}
+                        </div>
+                        <div class="col-2 block-example border border-dark">
+                            {(guess.probability_2 * 100).toFixed(2)}%
+                        </div>
+                        <div class="col-2 block-example border border-dark">
+                            {guess.guess_3}
+                        </div>
+                        <div class="col-2 block-example border border-dark">
+                            {(guess.probability_3 * 100).toFixed(2)}%
+                        </div>
+                    </div>
+                ))}
+
+            </div>
+
         </Default>
     );
 }
